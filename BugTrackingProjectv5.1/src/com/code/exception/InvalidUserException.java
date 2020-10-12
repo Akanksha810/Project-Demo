a6903bde07d48fef8bdae065eaed2be8cddc80b8
Akanksha <shivi.s98@gmail.com>
2020-10-12 20:42:26 +0530

    Testing

---
+++ BugTrackingProjectv5.1/src/com/code/exception/InvalidUserException.java
@@ -0,0 +1,13 @@
+//Author : Ankit
+//Purpose : Exception when user is invalid.
+package com.code.exception;
+
+public class InvalidUserException extends Exception {
+    
+	public InvalidUserException(message) 
+    { 
+        // Call constructor of parent Exception 
+        super(message); 
+    } 
+
+}
